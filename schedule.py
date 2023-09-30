e1d07c08b622a9cd79bbfd298357a19a47ba7e9f
Ryo Fujimura <ryo176999@gmail.com>
2023-09-29 20:53:55 -0700

    timeline view of classes

--- schedule.py
+++ schedule.py
@@ -1,24 +1,69 @@
 import json
 
+class_schedule_info = {
+    "341": [
+        [[1200, 1430], ["Friday"]],
+        [[800, 1030], ["Saturday"]],
+    ],
+    "325": [
+        [[1800, 1915], ["Monday", "Wednesday"]],
+        [[1700, 1815], ["Tuesday", "Thursday"]],
+    ],
+    "491A": [
+        [[1400, 1515], ["Monday", "Wednesday"]],
+        [[1930, 2045], ["Tuesday", "Thursday"]],
+        [[1830, 1945], ["Monday", "Wednesday"]],
+        [[2000, 2115], ["Tuesday", "Thursday"]],
+    ],
+    "326": [
+        [[1700, 1815], ["Tuesday", "Thursday"]],
+        [[1400, 1515], ["Monday", "Wednesday"]],
+    ],
+    "342": [
+        [[1230, 1345], ["Tuesday", "Thursday"]],
+        [[1400, 1515], ["Monday", "Wednesday"]],
+        [[1300, 1530], ["Friday"]],
+        [[1800, 1915], ["Monday", "Wednesday"]],
+        [[930, 1045], ["Tuesday", "Thursday"]],
+        [[800, 1045], ["Friday"]],
+        [[1830, 1945], ["Tuesday", "Thursday"]],
+    ],
+}
+
+personal_schedule = [
+    [[800, 2000], ["Saturday"]],
+    [[1000, 2000], ["Sunday"]],
+]
+
+'''
+This function generates all possible combinations of class sections that do not overlap with each other or the personal schedule using a backtracking approach. It calls the backtrack function to explore different combinations.
+'''
 def find_class_combinations(class_schedule_info, personal_schedule):
+    '''
+    This is a recursive function that explores possible combinations of class sections. It keeps track of the current combination and adds valid sections to it. When it reaches the end of the class schedule information, it appends the current combination to the list of combinations
+    '''
     def backtrack(index, current_combination):
         if index == len(class_schedule_info):
             combinations.append(current_combination.copy())
             return
-
         class_name, class_sections = list(class_schedule_info.items())[index]
-
+        # print(class_name,"------" ,class_sections)
         for section in class_sections:
             if all(not overlap(sect, section) for sect in current_combination) and not overlap_personal_schedule(section):
                 current_combination.append(section)
                 backtrack(index + 1, current_combination)
                 current_combination.pop()
 
+    '''
+    This function checks whether two class sections overlap in terms of day and time.
+    '''
     def overlap(section1, section2):
         time1, day1 = section1
         time2, day2 = section2
         return day1 == day2 and (time1[0] <= time2[1] and time2[0] <= time1[1])
-
+    '''
+    This function checks whether a class section overlaps with the personal schedule.
+    '''
     def overlap_personal_schedule(section):
         for personal_section in personal_schedule:
             if overlap(section, personal_section):
@@ -30,41 +75,9 @@ def find_class_combinations(class_schedule_info, personal_schedule):
 
     return combinations
 
-class_schedule_info = {
-    "341": [
-        [[1200, 1430], "Friday"],
-        [[800, 81030], "Saturday"],
-    ],
-    "325": [
-        [[1800, 1915], "Monday"],
-        [[1700, 1815], "Tuesday"],
-    ],
-    "491A": [
-        [[1400, 1515], "Monday"],
-        [[1930, 2045], "Tuesday"],
-        [[1830, 1945], "Monday"],
-        [[2000, 2115], "Tuesday"],
-    ],
-    "326": [
-        [[1700, 1815], "Tuesday"],
-        [[1400, 1515], "Monday"],
-    ],
-    "342": [
-        [[1230, 1345], "Tuesday"],
-        [[1400, 1515], "Monday"],
-        [[1300, 1530], "Friday"],
-        [[1800, 1915], "Monday"],
-        [[930, 1045], "Tuesday"],
-        [[800, 1045], "Friday"],
-        [[1830, 1945], "Tuesday"],
-    ],
-}
-
-personal_schedule = [
-    [[1000, 2000], "Saturday"],
-    [[1000, 2000], "Sunday"],
-]
-
+'''
+This function calls find_class_combinations to find all combinations and then prints them to the console.
+'''
 def print_combinations():
     combinations = find_class_combinations(class_schedule_info, personal_schedule)
     for i, combination in enumerate(combinations):
@@ -72,7 +85,9 @@ def print_combinations():
         for j, section in enumerate(combination):
             print(f"  Class: {list(class_schedule_info.keys())[j]}, Day: {section[1]}, Time: {section[0]}")
         print()
-
+'''
+It stores the combinations in a JSON file named "output.json" instead of printing them.
+'''
 def output_combinations():
     # output combinations as a json file just like print_combinations()
     combinations = find_class_combinations(class_schedule_info, personal_schedule)
@@ -87,6 +102,7 @@ def output_combinations():
 
 
 def main():
+    print_combinations()
     output_combinations()
 
 if __name__ == "__main__":
--- script.js
+++ script.js
@@ -1,38 +1,65 @@
-// Define class_schedule_info as before
+// Define your schedules
+const schedules = [
+    {
+        "341": { "day": ["Friday"], "time": [1200, 1430] },
+        "325": { "day": ["Monday", "Wednesday"], "time": [1800, 1915] },
+        "491A": { "day": ["Monday", "Wednesday"], "time": [1400, 1515] },
+        "326": { "day": ["Tuesday", "Thursday"], "time": [1700, 1815] },
+        "342": { "day": ["Tuesday", "Thursday"], "time": [1230, 1345] }
+      },
+      {
+        "341": { "day": ["Friday"], "time": [1200, 1430] },
+        "325": { "day": ["Monday", "Wednesday"], "time": [1800, 1915] },
+        "491A": { "day": ["Monday", "Wednesday"], "time": [1400, 1515] },
+        "326": { "day": ["Tuesday", "Thursday"], "time": [1700, 1815] },
+        "342": { "day": ["Tuesday", "Thursday"], "time": [930, 1045] }
+      },
+];
 
-document.addEventListener('DOMContentLoaded', () => {
-    generateCombinations();
-});
+function createTimetable(schedule, scheduleNumber) {
+    const tableContainer = document.getElementById("scheduleTables");
 
-function generateCombinations() {
-    const personalScheduleInput = document.getElementById('personal-schedule-input');
-    const personalSchedule = personalScheduleInput.value;
+    // Create an <h2> heading for the schedule number
+    const h2 = document.createElement("h2");
+    h2.textContent = `Schedule ${scheduleNumber}`;
+    tableContainer.appendChild(h2);
 
-    // Send the personal schedule to the server and get combinations
-    fetch('/generate_combinations', {
-        method: 'POST',
-        body: JSON.stringify({ personalSchedule }),
-        headers: {
-            'Content-Type': 'application/json'
+    const table = document.createElement("table");
+    table.id = `schedule${scheduleNumber}`;
+
+    for (let i = 8; i <= 21; i++) {
+        const row = document.createElement("tr");
+        const timeCell = document.createElement("td");
+        timeCell.textContent = i < 10 ? "0" + i + ":00" : i + ":00";
+        row.appendChild(timeCell);
+
+        for (let j = 0; j < 7; j++) {
+            const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][j];
+            let cellContent = "";
+            let cellClass = "free"; // Default to free/busy
+
+            for (const classId in schedule) {
+                const classSchedule = schedule[classId];
+                if (classSchedule.day.includes(day) && classSchedule.time[0] <= i * 100 && classSchedule.time[1] >= i * 100) {
+                    cellContent = classId;
+                    cellClass = "busy"; // Mark as busy
+                    break;
+                }
+            }
+
+            const cell = document.createElement("td");
+            cell.className = cellClass;
+            cell.textContent = cellContent;
+            row.appendChild(cell);
         }
-    })
-    .then(response => response.json())
-    .then(data => displayCombinations(data));
+
+        table.appendChild(row);
+    }
+
+    tableContainer.appendChild(table);
 }
 
-function displayCombinations(combinations) {
-    const combinationsDiv = document.getElementById('combinations');
-    combinationsDiv.innerHTML = '';
-
-    combinations.forEach((combination, index) => {
-        const combinationDiv = document.createElement('div');
-        combinationDiv.innerHTML = `<strong>Combination ${index + 1}:</strong><br>`;
-
-        combination.forEach(section => {
-            const classInfo = `Class: ${section[1]}, Time: ${section[0][0]}-${section[0][1]}, Day: ${section[1]}`;
-            combinationDiv.innerHTML += classInfo + '<br>';
-        });
-
-        combinationsDiv.appendChild(combinationDiv);
-    });
+// Create timetables for each schedule
+for (let i = 0; i < schedules.length; i++) {
+    createTimetable(schedules[i], i + 1);
 }
