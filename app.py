3b9f0ee9ff17d9a90cab45239f60975fde0a0ddb
Ryo Fujimura <ryo176999@gmail.com>
2023-10-06 21:11:29 -0700

    show schedule based on input dynamic website working

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 # app.py
 from flask import Flask, render_template, request, jsonify
-from scripts.calculator import square_number
-from scripts.calculator import create_personal_schedule
+from scripts.calculator import square_number, create_personal_schedule, output_combinations
 
 app = Flask(__name__)
-
-# Define a dictionary to map day names to their numeric representation (e.g., Monday: 0, Tuesday: 1, etc.)
-day_mapping = {
-    "Monday": 0,
-    "Tuesday": 1,
-    "Wednesday": 2,
-    "Thursday": 3,
-    "Friday": 4,
-    "Saturday": 5,
-    "Sunday": 6,
+class_schedule = {
+    "341": [
+        [[1200, 1430], ["Friday"]],
+        [[800, 1030], ["Saturday"]]
+    ],
+    "325": [
+        [[1800, 1915], ["Monday", "Wednesday"]],
+        [[1700, 1815], ["Tuesday", "Thursday"]]
+    ],
+    "491A": [
+        [[1400, 1515], ["Monday", "Wednesday"]],
+        [[1930, 2045], ["Tuesday", "Thursday"]],
+        [[1830, 1945], ["Monday", "Wednesday"]],
+        [[2000, 2115], ["Tuesday", "Thursday"]]
+    ],
+    "326": [
+        [[1700, 1815], ["Tuesday", "Thursday"]],
+        [[1400, 1515], ["Monday", "Wednesday"]]
+    ],
+    "342": [
+        [[1230, 1345], ["Tuesday", "Thursday"]],
+        [[1400, 1515], ["Monday", "Wednesday"]],
+        [[1300, 1530], ["Friday"]],
+        [[1800, 1915], ["Monday", "Wednesday"]],
+        [[930, 1045], ["Tuesday", "Thursday"]],
+        [[800, 1045], ["Friday"]],
+        [[1830, 1945], ["Tuesday", "Thursday"]]
+    ]
 }
+
 
 @app.route('/')
 def index():
@@ -27,19 +45,17 @@ def calculate():
     result = square_number(input_number)
     return jsonify({'calculation': result})
 
-
 @app.route('/calculate_schedule', methods=['POST'])
 def calculate_schedule():
-    data = request.get_json()
-    start_time = data['startTime']
-    end_time = data['endTime']
-    days_of_week = data['daysOfWeek']
-    result = create_personal_schedule(start_time, end_time, days_of_week)
-    
-    # Calculate the result (you can customize this calculation)
-    # result = f"Schedule from {start_time} to {end_time} on {', '.join(days_of_week)}"
-
-    return jsonify({'personal_schedule': result})
+    try:
+        data = request.get_json()
+        start_time = data['startTime']
+        end_time = data['endTime']
+        days_of_week = data['daysOfWeek']
+        result = output_combinations(class_schedule, create_personal_schedule(start_time, end_time, days_of_week))
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)}),
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scripts/calculator.py
+++ scripts/calculator.py
@@ -3,11 +3,46 @@ def square_number(number):
     return number * number
 
 def create_personal_schedule(start_time, end_time, days_of_week):
-    print("createprsonalschedule")
-    json_personal_schedule = []
+    personal_schedule = []
     for day in days_of_week:
-        json_personal_schedule.append([[int(start_time), int(end_time)], [day]])
+        personal_schedule.append([[int(start_time), int(end_time)], [day]])
+    return personal_schedule
 
-    print(json_personal_schedule)
-    return json_personal_schedule
+def create_class_schedule_info(start_time, end_time, days_of_week):
+    pass
 
+def find_class_combinations(class_schedule_info, personal_schedule):
+    def backtrack(index, current_combination):
+        if index == len(class_schedule_info):
+            combinations.append(current_combination.copy())
+            return
+        class_name, class_sections = list(class_schedule_info.items())[index]
+        for section in class_sections:
+            if all(not overlap(sect, section) for sect in current_combination) and not overlap_personal_schedule(section):
+                current_combination.append(section)
+                backtrack(index + 1, current_combination)
+                current_combination.pop()
+    def overlap(section1, section2):
+        time1, day1 = section1
+        time2, day2 = section2
+        return day1 == day2 and (time1[0] <= time2[1] and time2[0] <= time1[1])
+    def overlap_personal_schedule(section):
+        time, day = section
+        for personal_section in personal_schedule:
+            for d in day:
+                if d in personal_section[1] and (time[0] <= personal_section[0][1] and personal_section[0][0] <= time[1]):
+                    return True
+        return False
+    combinations = []
+    backtrack(0, [])
+    return combinations
+
+def output_combinations(class_schedule_info, personal_schedule):
+    combinations = find_class_combinations(class_schedule_info, personal_schedule)
+    output = []
+    for i, combination in enumerate(combinations):
+        combination_dict = {}
+        for j, section in enumerate(combination):
+            combination_dict[list(class_schedule_info.keys())[j]] = {"day": section[1], "time": section[0]}
+        output.append(combination_dict)
+    return output
